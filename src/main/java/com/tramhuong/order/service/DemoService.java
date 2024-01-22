d38add051363ee270e0bf40b35f5289a5b2badc3
Truongcr <truongcr1609@gmail.com>
2024-01-22 21:09:43 +0700

    update order

---
+++ src/main/java/com/tramhuong/order/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.tramhuong.order.service;
+
+import com.tramhuong.common.utils;
+////import com.tramhuong.common.utils.bases3.FileCommonUtils;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+
+//@Service
+//@RequiredArgsConstructor
+public class DemoService {
+
+
+
+//    private final Test fileCommonUtils;
+
+//    public DemoService(Test fileCommonUtils) {
+//        this.fileCommonUtils = fileCommonUtils;
+//    }
+}
